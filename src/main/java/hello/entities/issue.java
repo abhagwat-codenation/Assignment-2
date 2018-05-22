66548c0cc98258729c18a944fe0d6a164419f5df
aditya <aditya@Adityas-MacBook-Pro.local>
2018-05-23 05:12:11 +0530

    the whole thing

---
+++ src/main/java/hello/entities/issue.java
@@ -0,0 +1,52 @@
+//package hello.entities;
+//
+//public class issue {
+//
+//    public void setIssueID(Long issueID) {
+//        this.issueID = issueID;
+//    }
+//
+//    public void setIssueType(Integer issueType) {
+//        this.issueType = issueType;
+//    }
+//
+//    public void setLine(Integer line) {
+//        this.line = line;
+//    }
+//
+//    public void setFile(Integer file) {
+//        this.file = file;
+//    }
+//
+//    public void setCol(Integer col) {
+//        this.col = col;
+//    }
+//
+//    public Long getIssueID() {
+//
+//        return issueID;
+//    }
+//
+//    public Integer getIssueType() {
+//        return issueType;
+//    }
+//
+//    public Integer getLine() {
+//        return line;
+//    }
+//
+//    public Integer getFile() {
+//        return file;
+//    }
+//
+//    public Integer getCol() {
+//        return col;
+//    }
+//
+//    Long issueID;
+//    Integer issueType;
+//    Integer line;
+//    Integer file;
+//    Integer col;
+//
+//}
